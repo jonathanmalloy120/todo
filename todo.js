e78880c253a5edf872a33e30e6e5b38053c390fe
Jon Malloy <jonathan.malloy3@gmail.com>
2021-12-06 15:13:04 -0500

    added some basic styling

--- todo.js
+++ todo.js
@@ -1,11 +1,13 @@
 function Todo({todo,index,remove}) {
+    var counter = 1;
     function handle() {
         remove(index);
     }
 
-    return (
-        <div className = "todo" onClick={handle} /*removes this todo on click*/>
-        {todo.text} (-) </div>
-    )
+        return (
+            <div className = "todo" onClick={handle} style={{background: '#a9a9a9'}}/*removes this todo on click*/>
+            {todo.text} (-) </div>
+        )
+
     
 }
